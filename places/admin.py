c469d7ca0ed4c4b9411867fa05e4a2101b03de7b
Konstantin Beloborodov <kosta.white@gmail.com>
2022-06-01 14:53:13 +0300

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Place
+from .models import Place, Photo
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
     pass
--- places/models.py
+++ places/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 
 
-# Create your models here.
 class Place(models.Model):
     title = models.CharField('Название', max_length=200, db_index=True)
     description_short = models.TextField(
         'Краткое описание', null=True, blank=True)
     description_long = models.TextField(
         'Полное описание', null=True, blank=True)
-    lng = models.DecimalField('Долгота',
-        max_digits=17, decimal_places=14, null=True, blank=True)
-    lat = models.DecimalField('Широта',
-        max_digits=17, decimal_places=14, null=True, blank=True)
+    lng = models.DecimalField(
+        'Долгота',max_digits=17, decimal_places=14, null=True, blank=True)
+    lat = models.DecimalField(
+        'Широта',max_digits=17, decimal_places=14, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Место'
@@ -19,3 +18,18 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Photo(models.Model):
+    num_order = models.PositiveSmallIntegerField(
+        'Порядковый номер', help_text='1 - обложка достопримечательности')
+
+    image = models.ImageField('Картинка', upload_to='uploads/')
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Фотография'
+        verbose_name_plural = 'Фотографии мест'
+
+    def __str__(self):
+        return f'{self.num_order} {self.place.title}'
